3ad3f8ae106a75fd1db4cb1a5c15189503624f0f
juhanse <julien.hanse12@gmail.com>
2025-01-17 17:46:20 +0100

    delete: isEmpty password

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,6 +1,5 @@
 export class PasswordChecker {
 	isValid(password) {
-		if (password.isEmpty()) return false;
 		if (password.length < 8) return false;
 		if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false;
 		if (!/\d/.test(password)) return false;
